22de7e9ff89f1816cb908476e25d1513fdd3d216
Daroface <dominik1050@gmail.com>
2020-03-12 00:02:15 +0100

    Create project
    Create project which print hello message.

---
+++ src/com/daroface/Main.java
@@ -0,0 +1,10 @@
+package com.daroface;
+
+public class Main {
+    private static String helloMessage = "Hello Sir! My name is Clark.";
+    private static String birthYearMessage = "I was created in 2020.";
+    public static void main(String[] args) {
+        System.out.println(helloMessage);
+        System.out.println(birthYearMessage);
+    }
+}
