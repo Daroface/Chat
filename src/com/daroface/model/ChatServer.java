30a26c77aefdc1f4dce97c052f6ac945bf5ddad6
Daroface <dominik1050@gmail.com>
2020-03-24 11:49:36 +0100

    Create chat server.

--- src/com/daroface/model/ChatServer.java
+++ src/com/daroface/model/ChatServer.java
@@ -1,10 +1,39 @@
 package com.daroface.model;
-//test git
-public class ChatModel {
-    private static String helloMessage = "Hello Sir! My name is Clark.";
-    private static String birthYearMessage = "I was created in 2020.";
-    public static void main(String[] args) {
-        System.out.println(helloMessage);
-        System.out.println(birthYearMessage);
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class ChatServer extends Thread {
+    private static final int port = 6870;
+    private DatagramSocket socket;
+    private boolean isRunning;
+    private byte[] buffer = new byte[256];
+
+    public ChatServer() throws SocketException {
+        socket = new DatagramSocket(port);
+        isRunning = false;
+    }
+
+    public void run() {
+        isRunning = true;
+        while (isRunning) {
+            try {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                InetAddress address = packet.getAddress();
+                int clientPort = packet.getPort();
+                String received = new String(packet.getData(), 0, packet.getLength());
+                if (received.equals("end")) {
+                    isRunning = false;
+                    continue;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        socket.close();
     }
 }
