9895a1a68c650f61837efad47a824bb429a9f9d8
Daroface <dominik1050@gmail.com>
2020-03-24 17:29:59 +0100

    Server commands and clients collection
    Add special server commands and create collection of connected
    clients.

--- src/com/daroface/model/ChatServer.java
+++ src/com/daroface/model/ChatServer.java
@@ -5,14 +5,17 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 public class ChatServer extends Thread {
     private static final int port = 6870;
     private DatagramSocket socket;
     private boolean isRunning;
     private byte[] buffer = new byte[256];
+    ArrayList<Client> clients;
 
     public ChatServer() throws SocketException {
+        clients = new ArrayList<Client>();
         socket = new DatagramSocket(port);
         isRunning = false;
     }
@@ -25,17 +28,41 @@ public class ChatServer extends Thread {
                 socket.receive(packet);
                 InetAddress address = packet.getAddress();
                 int clientPort = packet.getPort();
-                packet = new DatagramPacket(buffer, buffer.length, address, clientPort);
-                String received = new String(packet.getData(), 0, packet.getLength());
-                if (received.equals("end")) {
-                    isRunning = false;
-                    continue;
+                String receivedMessage = new String(packet.getData(), 0, packet.getLength());
+                if(execute(receivedMessage, packet)) {
+                    buffer = "Ok".getBytes();
                 }
+                packet = new DatagramPacket(buffer, buffer.length, address, clientPort);
                 socket.send(packet);
+                buffer = new byte[256];
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         socket.close();
+    }
+
+    private boolean execute(String command, DatagramPacket packet) {
+        switch(command) {
+            case "end":
+                isRunning = false;
+                return true;
+            case "addMe":
+                if(!isClientExist(packet.getAddress())) {
+                    clients.add(new Client(packet.getAddress(), packet.getPort(), clients.size()));
+                    clients.get(clients.size()-1);
+                }
+                return true;
+        }
+        return false;
+    }
+
+    private boolean isClientExist(InetAddress address) {
+        for(Client client : clients) {
+            if(client.getAddress().equals(address)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
